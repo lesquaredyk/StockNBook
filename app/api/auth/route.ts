baeb67b008b32df6fe25f3d9371b1828de512c36
Ellise James Tamayo <ellisejamesbtamayo@iskolarngbayan.pup.edu.ph>
2026-04-08 10:28:47 +0800

    Updated POC

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -4,7 +4,7 @@ const LAMBDA_URL = "https://7oxhafersb.execute-api.ap-southeast-1.amazonaws.com"
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const path = body.action === "login" ? "/login" : "/register";
+  const path  = body.action === "login" ? "/login" : "/register";
 
   const response = await fetch(`${LAMBDA_URL}${path}`, {
     method: "POST",
